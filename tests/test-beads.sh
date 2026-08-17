de776ed8ef1d128e9680a986bc47ce9160425d57
Angelo Oparah <16281580+Angel-O@users.noreply.github.com>
2026-08-17 21:16:48 +0100

    beads: pin merged Viewer integration

--- tests/test-beads.sh
+++ tests/test-beads.sh
@@ -6,7 +6,7 @@ root=$2
 case_root=$root/beads-installer
 enabled=$root/personal-with-beads/rendered/run_after_15-install-beads-viewer-fork.sh.tmpl
 disabled=$root/personal/rendered/run_after_15-install-beads-viewer-fork.sh.tmpl
-expected_ref=945052bbb12b77ddf3ab9381d8f5f150b4c5293e
+expected_ref=8aa6bb144aa506176bb534d2e150eefa620f1340
 
 fail() {
   printf 'beads test: %s\n' "$*" >&2
