d7a9433bbc51d170435bbb30a020c5cf39bfd10d
Angelo Oparah <16281580+Angel-O@users.noreply.github.com>
2026-08-25 08:44:02 +0100

    Pass Docker Git trust through environment

--- tests/run-docker.sh
+++ tests/run-docker.sh
@@ -5,5 +5,8 @@ repo=$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)
 image=portable-dotfiles-test
 
 docker build --tag "$image" --file "$repo/tests/Dockerfile" "$repo"
-docker run --rm --volume "$repo:/src:ro" "$image" \
-  sh -c 'git config --global --add safe.directory /src && exec bash tests/test.sh'
+docker run --rm --volume "$repo:/src:ro" \
+  --env GIT_CONFIG_COUNT=1 \
+  --env GIT_CONFIG_KEY_0=safe.directory \
+  --env GIT_CONFIG_VALUE_0=/src \
+  "$image"
